9ad0b1c496011e42d3a2180cd900ca686a766d8c
FTS18 <73402363+FTS18@users.noreply.github.com>
2021-01-09 22:42:54 +0530

    Create main.js

---
+++ js/main.js
@@ -0,0 +1,16 @@
+const toTop = document.querySelector(".to-top");
+
+window.addEventListener("scroll", () => {
+
+  if (window.pageYOffset > 100) {
+
+    toTop.classList.add("active");
+
+  } else {
+
+    toTop.classList.remove("active");
+
+  }
+
+})
+
