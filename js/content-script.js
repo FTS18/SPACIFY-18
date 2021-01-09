9e25553e40b600de2d95835f809cbb05f3d5ea55
FTS18 <73402363+FTS18@users.noreply.github.com>
2021-01-09 22:42:20 +0530

    Create content-script.js

---
+++ js/content-script.js
@@ -0,0 +1,27 @@
+function notifyExtension(e) {
+
+  var target = e.target;
+
+  while ((target.tagName != "A" || !target.href) && target.parentNode) {
+
+    target = target.parentNode;
+
+  }
+
+  if (target.tagName != "A")
+
+    return;
+
+  console.log("Content script sending message");
+
+  browser.runtime.sendMessage({"url": target.href});
+
+}
+
+/*
+
+Add notifyExtension() as a listener to click events.
+
+*/
+
+window.addEventListener("click", notifyExtension);
