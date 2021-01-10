ce0430302cbbfe8709dd7e185637ada4fad60a6d
Falcon Tech Studios <73402363+FTS18@users.noreply.github.com>
2021-01-10 14:33:16 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -5,6 +5,8 @@ function onSignIn(googleUser)
     var profile=googleUser.getBasicProfile();
 
     $(".g-signin2").css("display","none");
+    
+    $(".fb-btn").css("display","none");
 
     $(".data").css("display","block");
 
