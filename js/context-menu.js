a2096462fc21fff4954eb9fda2daea80708c099c
FTS18 <anandsagardubey71568@gmail.com>
2021-01-22 16:06:59 +0530

    ss

---
+++ js/context-menu.js
@@ -0,0 +1,25 @@
+window.addEventListener("contextmenu", function(event) {
+  event.preventDefault();
+  var contextElement = document.getElementById("context-menu");
+  contextElement.style.top = event.offsetY + "px";
+  contextElement.style.left = event.offsetX + "px";
+  contextElement.classList.add("active");
+})
+window.addEventListener("click", function() {
+  document.getElementById("context-menu").classList.remove("active");
+})
+
+function reload() {
+  location.reload();
+}
+
+function exit() {
+  self.close();
+}
+
+function inspectEl() {
+  javascript: (function() { var script = document.createElement('script');
+    script.src = "//cdn.jsdelivr.net/npm/eruda";
+    document.body.appendChild(script);
+    script.onload = function() { eruda.init() } })();
+}
