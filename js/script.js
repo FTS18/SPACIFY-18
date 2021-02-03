785c6356b992d5139f1b5a9a53a393763df1c3ff
FTS18 <anandsagardubey71568@gmail.com>
2021-02-03 19:28:56 +0530

    ggop

--- js/script.js
+++ js/script.js
@@ -3,6 +3,7 @@ function onSignIn(googleUser)
 {
   var profile = googleUser.getBasicProfile();
   $(".g-signin2").css("display", "none");
+  $("#container1").css("display", "none");
   $(".fb-btn").css("display", "none");
   $(".data").css("display", "block");
   $("#pic").attr("src", profile.getImageUrl());
