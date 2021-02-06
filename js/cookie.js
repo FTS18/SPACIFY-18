d0a9f22410d30a308568b1169fddef0830ef3fc5
ggop <anandsagardubey71568@gmail.com>
2021-02-06 09:59:26 +0530

    ggop

--- js/cookie.js
+++ js/cookie.js
@@ -1,4 +1,4 @@
-const storageType = localStorage;
+const storageType = sessionStorage;
 const consentPropertyName = 'jdc_consent';
 
 const shoulShowPopup = () => !storageType.getItem(consentPropertyName);
@@ -6,16 +6,17 @@ const saveToStorage = () => storageType.getItem(consentPropertyName, true);
 
 function consentPopup(){
 
-  const consentPopup=document.getElementById("consent-popup");
+  const consentPopup=document.getElementById("consent");
   if (shoulShowPopup()) {
     const consent = confirm('Agree to the terms and Conditions of our website?');
     if (consent) {
       saveToStorage();
       console.log('You have accepted our Cookie Consent Popup');
+      consentPopup.classList.add("none");
     } else {
       console.log('You Dismissed the Cookie Consent Popup');
     }
   } else {
-
+    consentPopup.classList.add("none");
   }
 }
